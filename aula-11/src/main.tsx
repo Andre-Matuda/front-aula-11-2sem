936fc7cf89b4b485009dbbd939dce1bf96830fca
Andre-Matuda <asmatuda@outlook.com>
2026-04-27 11:30:09 -0300

    feat: Rotas criadas com sucesso e funcionais

--- aula-11/src/main.tsx
+++ aula-11/src/main.tsx
@@ -1,9 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import Error from "./routes/Error/index.tsx";
+import Home from "./routes/Home/index.tsx";
+import Users from "./routes/Users/index.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/users", element: <Users /> },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}/>
+  </StrictMode>
+);
---
+++ aula-11/src/routes/Error/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Error() {
+  return (
+    <main>
+        <h1>Error 404 - Não Encontrado</h1>
+        <div>
+            <p>Voltar para o inicio</p>
+        </div>
+    </main>
+  )
+}
